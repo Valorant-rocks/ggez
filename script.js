adc6b9542f68f50a80cefeaa8aee8d89ef792ed1
Adish <adishuprety89@gmail.com>
2023-11-28 08:36:42 +0545

    Final

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-
+//short load
 // Scroll to the top of the page when the page loads or refreshes
-
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry);
     if (entry.isIntersecting || entry.intersectionRatio > 0) {
       entry.target.classList.add('show');
-      observer.unobserve(entry.target); // Stop observing after making it visible
     }
   });
+}, {
+  rootMargin: '0px 0px 500px 0px' // Adjust the margin as per requirement
 });
 
 const hiddenElements = document.querySelectorAll('.disW');
 hiddenElements.forEach((el) => observer.observe(el));
 
-
 window.onload = function() {
   window.scrollTo(0, 0);
 };
+
+
+//especially for "what can it do?"
+
+
+
 
 
 
